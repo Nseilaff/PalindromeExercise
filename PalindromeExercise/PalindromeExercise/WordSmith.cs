49f852aca6664aa299da7a8eaad4c7068c5362ca
Nate Seilaff <seilaffn@gmail.com>
2022-08-30 20:01:15 -0600

    First Commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+
+            return str?.ToLower().SequenceEqual(str.Reverse()) ?? false;
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,27 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("tenet", true)]
+        [InlineData(null, false)]
+        [InlineData("otto", true)]
+        [InlineData("Nate", false)]
+        [InlineData("HOME", false)]
+        [InlineData("nurserun", false)]
+        [InlineData("civic", true)]
+        [InlineData("evilolive", true)]
+        [InlineData("homerun", false)]
+        public void PlaindromeTest(string str, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(str);
+            //Assert
+            Assert.Equal(expected, actual);
+            
         }
+        
     }
 }
